cee35f6f2708065ce060c670aa1f02d6df3b9468
Matilda Mared <matildamared@live.se>
2023-02-03 08:51:15 +0100

    update game logic

--- src/MarkersGame/MarkersGame.ts
+++ src/MarkersGame/MarkersGame.ts
@@ -1,20 +1,78 @@
 import { Marker } from "../Marker/Marker";
+import { Rule } from "../Marker/Rules/Rule";
 import { MarkerBag } from "../MarkerBag/MarkerBag";
 
 export class MarkersGame {
 	private markerBag: MarkerBag = new MarkerBag();
 	private pickedMarkers: Marker[] = [];
+	private gameRunning: boolean = false;
+	private rules: Rule[] = [];
 
-	constructor() {}
+	constructor(rules: Rule[]) {
+		this.rules = rules;
+	}
 
-	startGame(): void {
+	setupNewGame(): void {
 		console.log("Welcome to the colored markers game! Let's start! 😄");
 		console.log("Generating markers...");
 
 		this.markerBag = new MarkerBag();
 		this.pickedMarkers = [];
+		this.gameRunning = true;
+
 		console.log("Markers generated! 🎉");
-		console.log(this.markerBag);
-		console.log(this.pickedMarkers);
+	}
+
+	playGame(): void {
+		this.setupNewGame();
+
+		while (this.gameRunning) {
+			const pickedMarker = this.markerBag.pickRandomMarker();
+
+			if (pickedMarker) {
+				this.pickedMarkers.push(pickedMarker);
+				console.log(
+					`You picked a ${pickedMarker.color} marker with value ${pickedMarker.value}! 🎨 🎲`
+				);
+			} else {
+				this.gameOver();
+			}
+
+			this.rules.forEach((rule) => {
+				if (rule.applyRule(this.pickedMarkers)) {
+					rule.handleRule(this.pickedMarkers, this.markerBag);
+				}
+			});
+
+			const whiteMarkersValue = this.pickedMarkers
+				.filter((marker) => marker.color === "white")
+				.reduce((total, marker) => total + marker.value, 0);
+
+			if (whiteMarkersValue > 7) {
+				console.log(
+					"The value of the white markers exceeds 7 so the game is over! 😢"
+				);
+				this.gameOver();
+			}
+		}
+	}
+
+	gameOver(): void {
+		this.gameRunning = false;
+
+		const totalScore = this.pickedMarkers.reduce(
+			(total, marker) => total + marker.value,
+			0
+		);
+
+		console.log(`Your total score is ${totalScore}! 🎉`);
+
+		console.log("--------------------");
+
+		console.log("Your picked markers:");
+
+		this.pickedMarkers.forEach((marker) => {
+			console.log(`- ${marker.color} marker with value ${marker.value}! 🎨`);
+		});
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import { RubyRule } from "./Marker/Rules/RubyRule";
+import { WhiteMarkerAfterYellowRule } from "./Marker/Rules/WhiteMarkerAfterYellowRule";
+import { WhiteMarkersAboveNineRule } from "./Marker/Rules/WhiteMarkersAboveNineRule";
 import { MarkersGame } from "./MarkersGame/MarkersGame";
 
-const markersGame = new MarkersGame();
-markersGame.startGame();
-console.log("Hello!");
+const markersGame = new MarkersGame([
+	new RubyRule(),
+	new WhiteMarkerAfterYellowRule(),
+	new WhiteMarkersAboveNineRule(),
+]);
+
+markersGame.playGame();
