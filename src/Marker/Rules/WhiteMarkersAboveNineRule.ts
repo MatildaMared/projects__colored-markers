bed84c641eff03ee707257c85b24f80eb1570125
Matilda Mared <matildamared@live.se>
2023-02-03 08:41:54 +0100

    add white markers above nine rule

---
+++ src/Marker/Rules/WhiteMarkersAboveNineRule.ts
@@ -0,0 +1,32 @@
+import { MarkerBag } from "../../MarkerBag/MarkerBag";
+import { Marker } from "../Marker";
+import { Rule } from "./Rule";
+
+export class WhiteMarkersAboveNineRule implements Rule {
+	applyRule(pickedMarkers: Marker[]): boolean {
+		const whiteMarkersTotalValue = pickedMarkers
+			.filter((marker) => marker.color === "white")
+			.reduce((total, marker) => total + marker.value, 0);
+
+		const ruleApplies = whiteMarkersTotalValue > 9;
+
+		if (ruleApplies) {
+			console.log(
+				"The total value of white markers is above 9, so the WhiteMarkersAboveNine rule applies!"
+			);
+		}
+
+		return ruleApplies;
+	}
+
+	handleRule(pickedMarkers: Marker[], markerBag: MarkerBag): void {
+		const allGreenMarkers = pickedMarkers.filter(
+			(marker) => marker.color === "green"
+		);
+		allGreenMarkers.forEach((marker) => {
+			const markerIndex = pickedMarkers.indexOf(marker);
+			pickedMarkers.splice(markerIndex, 1);
+			markerBag.addMarker(marker);
+		});
+	}
+}
